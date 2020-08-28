07609a2472ed37fa2d9599813f043d001349efeb
cristopher Palacios <cristopher@fanatiz.com>
2020-08-28 15:04:30 -0400

    setup eslint global config

--- test/globals.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const chai = require('chai'),
-    Path = require('wrapper-path');
-
-global._expect = chai.expect;
-global._path = new Path(`${__dirname}/../`);
--- test/index.test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-describe('Unit Testing', () => {
-    require('./unit');
-});
